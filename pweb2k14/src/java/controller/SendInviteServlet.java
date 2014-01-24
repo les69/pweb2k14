0d7f295d95ed4c4983559437f0109437a3ecc27d
TheFerrango <llorenzo@outlook.com>
2014-01-24 16:44:34 +0100

    groundwork
    *prepared servlets foar features
    *EXTERMINATEEE!

--- pweb2k14/src/java/controller/SendInviteServlet.java
+++ pweb2k14/src/java/controller/SendInviteServlet.java
@@ -6,30 +6,46 @@
 
 package controller;
 
-import helpers.ServletHelperClass;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.DbHelper;
-import model.Group;
-import model.User;
 
 /**
  *
  * @author lorenzo
  */
-public class NewGroupServlet extends HttpServlet {
-private DbHelper helper;
+public class SendInviteServlet extends HttpServlet {
 
-    @Override
-    public void init() throws ServletException {
-        this.helper = (DbHelper) super.getServletContext().getAttribute("dbmanager");
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SendInviteServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SendInviteServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
-   
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -41,7 +57,7 @@ private DbHelper helper;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.sendRedirect("MyGroup/newGroupForm.jsp");
+        processRequest(request, response);
     }
 
     /**
@@ -55,16 +71,7 @@ private DbHelper helper;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String groupName = request.getParameter("grpName");
-        boolean pgroup = (request.getParameterValues("pubSelector")!= null);
-        User user = ServletHelperClass.getUserFromSession(request);
-        Group newG = new Group();
-        newG.setName(groupName);
-        newG.setPublic(pgroup);
-        newG.setOwner(user.getId());
-        helper.addGroup(newG);
-        helper.addUserToGroup(newG, user);
-        response.sendRedirect("CyberController?oper=getMyGroups");
+        processRequest(request, response);
     }
 
     /**
@@ -75,5 +82,6 @@ private DbHelper helper;
     @Override
     public String getServletInfo() {
         return "Short description";
-    }
+    }// </editor-fold>
+
 }
